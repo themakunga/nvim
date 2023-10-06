79bec6af0cf47cd1fbd8bab6d2f2896e40eed5bd
nicolas martinez v <nmartinezv@icloud.com>
2023-10-06 11:04:26 -0300

    fix update golang

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -2,7 +2,7 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
     if type(opts.sources) == "table" then
-      local nls = require("none-ls")
+      local nls = require("null-ls")
 
       vim.list_extend(opts.sources, {
         nls.builtins.code_actions.gomodifytags,

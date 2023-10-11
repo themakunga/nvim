6c56e21b6f05792f96b7bd05e4be4e37a3a755c5
Nicolas Martinez <nmartinezv@icloud.com>
2023-10-11 12:26:20 -0300

    feat: paste command

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,17 +6,18 @@
 -- NeoVide Setup
 if vim.g.neovide then
   local conf = vim.g
-  local map = vim.api.nvim_set_keymap
 
   conf.neovide_remember_window_size = true
   conf.neovide_confirm_quit = true
   conf.neovide_fullscreen = true
   conf.neovide_input_use_logo = 1
-
-  map("", "<D-v>", "+p<CR>", { noremap = true, silent = true, desc = "Paste" })
-  map("!", "<D-v>", "<C-R>+", { noremap = true, silent = true, desc = "Paste" })
-  map("t", "<D-v>", "<C-R>+", { noremap = true, silent = true, desc = "Paste" })
-  map("v", "<D-v>", "<C-R>+", { noremap = true, silent = true, desc = "Paste" })
 end
 
 vim.opt.swapfile = false
+
+-- paste data to buffer
+local map = vim.api.nvim_set_keymap
+map("", "<D-v>", "+p<CR>", { noremap = true, silent = true, desc = "Paste" })
+map("!", "<D-v>", "<C-R>+", { noremap = true, silent = true, desc = "Paste" })
+map("t", "<D-v>", "<C-R>+", { noremap = true, silent = true, desc = "Paste" })
+map("v", "<D-v>", "<C-R>+", { noremap = true, silent = true, desc = "Paste" })
